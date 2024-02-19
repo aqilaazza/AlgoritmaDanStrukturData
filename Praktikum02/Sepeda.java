98a40e04f70ac47060c6a5f23b7ac020f2039a95
aqilaazza <aqilanurazza123@gmail.com>
2024-02-19 15:40:38 +0700

    commit

---
+++ Praktikum02/Sepeda.java
@@ -0,0 +1,33 @@
+package Praktikum02;
+
+public class Sepeda {
+    float kecepatan;
+    int gear;
+
+    public Sepeda(float newKecepatan, int newGear) {
+      kecepatan = newKecepatan;
+      gear = newGear;
+    }
+
+    public void tambahKecepatan (float increment) {
+       if (kecepatan + increment <= 20) {
+            kecepatan += increment;
+       } else {
+        kecepatan = 20;
+       } 
+    }
+
+    public void kurangiKecepatan (float decrement) {
+    if (kecepatan - decrement >= 0) {
+        kecepatan -= decrement;
+     } else {
+        kecepatan = 0;
+       }
+    }
+
+    public void cetakInfo() {
+        System.out.println("Kecepatan : " +kecepatan);
+        System.out.println("Gear : " +gear);
+        System.out.println("======================");
+    }
+ }
